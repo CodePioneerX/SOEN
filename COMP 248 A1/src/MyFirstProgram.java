b0b07101d0cf47d4594965e73d4dd240bb4a8bdf
haris2055 <78054284+haris2055@users.noreply.github.com>
2022-01-11 02:35:22 -0500

    Add files via upload

---
+++ COMP 248 A1/src/MyFirstProgram.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+// -----------------------------------------------------
+// Assignment 1
+// Written by: Haris Mahmood, ID: 40135271
+// -----------------------------------------------------
+
+
+						//*****************************************
+						//*				WELCOME 				  *
+						//*****************************************
+
+public class MyFirstProgram 
+{
+
+	public static void main(String[] args) 
+	{
+		//---------------------------------------------------------------------------------------------------
+		//Part 1
+		// * This is a basic program that utilizes hard coded lines to display an output. *
+		
+		//System.out.println --> used to produce statements that can be displayed in the output (Device)
+		System.out.println("Welcome to My First Java Program!");
+		System.out.println("This Program was written by: Haris Mahmood");
+		System.out.println("The assignment was done individually on Friday December 31, 2021 at 6:43:12 PM.");
+		System.out.println("End of Program!");
+		//---------------------------------------------------------------------------------------------------
+		
+		//---------------------------------------------------------------------------------------------------
+		//Part 2
+		// * This is a basic program that take input from user and based on the input calculates the various apps and songs they can purchase.
+
+		
+		Scanner keybord= new Scanner(System.in);
+		System.out.println("\nHow much money do you wish to prepay?");
+		int money= keybord.nextInt();
+		
+		System.out.println("With this amount, you will be able to purchase " + (money/3) + " app(s). You will then have "+ (money%3) +"$ left as a credit on your account.");
+		int apps= (money/7);
+		int remainingAmount= money-(apps*7);
+		int amountOfApps= remainingAmount/3;
+		int remainingBalance= remainingAmount%3;
+		System.out.println("Alternatively, for this amount, you will be able to purchase "+ apps + " song(s) and "+ amountOfApps + " app(s). You will then have "+ remainingBalance +"$ left as a credit on your\n" + "account.");
+		System.out.println("Thanks for Shopping at Best Music Store!");
+		//---------------------------------------------------------------------------------------------------
+		
+	}
+
+}
+
+// End of Program
+
+
---
+++ COMP 248 A2/src/CashierCalculator.java
@@ -0,0 +1,110 @@
+import java.util.Scanner;
+// -----------------------------------------------------
+// Assignment 2
+// Written by: Haris Mahmood, ID: 40135271
+// -----------------------------------------------------
+
+
+						//*****************************************
+						//*				WELCOME 				  *
+						//*****************************************
+
+/* Calculator that asks for the user to enter number of items with a given limit, price of those items within
+the limit, GST and QST of the products within a given limit. It then provides the calculation of what will be the total amount.*/
+
+public class CashierCalculator 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner keyboard = new Scanner(System.in);			//Taking users inputs
+		int countMistakes=0;								//Counts total number of incorrect values entered
+
+		int numberOfItems=0;								
+		final int minItems=1;
+		final int maxItems=10;
+		if(numberOfItems<minItems || numberOfItems> maxItems)
+		{
+			System.out.printf("Please enter the number of items bought [1...10]: ");
+			numberOfItems=keyboard.nextInt();
+			
+			while(numberOfItems<minItems|| numberOfItems>maxItems)					//It will only enter here if in above the value enter was not satisfactory
+			{
+				System.out.printf("Please enter the number of items bought [1...10]: ");
+				numberOfItems=keyboard.nextInt();
+				countMistakes++;														//Increments the variable value to account for error
+			}
+			
+		}
+		
+		int[] priceOfItems = new int[numberOfItems];				//An array with size of the number of items that stores the prices
+		final int minPrice=1; 
+		final int maxPrice=1000;
+		for(int i=0; i<priceOfItems.length;i++)
+		{
+			System.out.printf("Please enter the price of item "+(i+1) + " [1...1000]: ");
+			priceOfItems[i]=keyboard.nextInt();
+			
+			while(priceOfItems[i]<minPrice || priceOfItems[i]>maxPrice)
+			{
+				System.out.printf("Please enter the price of item "+(i+1) + " [1...1000]: ");
+				priceOfItems[i]=keyboard.nextInt();
+				countMistakes++;
+			}
+		}
+		
+		float gst= -1;
+		final int minGst=0;
+		final int maxGst=14;
+		if(gst<minGst || gst>maxGst)
+		{
+			System.out.printf("Please enter the tax rate of GST in %% [0...14]: ");
+			gst=keyboard.nextFloat();
+			
+			while(gst<minGst|| gst>maxGst)
+			{
+				System.out.printf("Please enter the tax rate of GST in %% [0...14]: ");
+				gst=keyboard.nextFloat();
+				countMistakes++;
+			}
+		}
+		
+		float qst= -1;
+		final int minQst=0;
+		final int maxQst=17;
+		if(qst<minQst || qst>maxQst)
+		{
+			System.out.printf("Please enter the tax rate of QST in %% [0...17]: ");
+			qst=keyboard.nextFloat();
+			
+			while(qst<minQst || qst>maxQst)
+			{
+				System.out.printf("Please enter the tax rate of QST in %% [0...17]: ");
+				qst=keyboard.nextFloat();
+				countMistakes++;
+			}
+		}
+		
+		float subTotal=0;
+		
+		for(int i=0; i<priceOfItems.length;i++)
+		{
+			subTotal+=priceOfItems[i];
+		}
+		
+		float subGst= ((subTotal) * (gst/100));
+		float subQst= ((subTotal) * (qst/100));
+		float total=subTotal + subGst +subQst;
+		
+		if(countMistakes!=0)			//Condition if there's no errors made then the message wont display
+		{
+			System.out.printf("You have entered %d invalid inputs.%n",countMistakes);
+		}
+		
+		System.out.printf("Sub Total: 	    $ %6.2f%n",subTotal);
+		System.out.printf("GST: 		    $ %6.2f%n",subGst);
+		System.out.printf("QST: 		    $ %6.2f%n",subQst);
+		System.out.printf("Total: 		    $ %6.2f%n",total);
+	}
+
+}
